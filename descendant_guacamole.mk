4bdc8a41a793aad043dfc036432c16338196c63d
Alexandria C <alexandria.cortez@linuxandria.com>
2020-08-28 16:30:13 +0200

    Initial Descendant bringup
    Signed-off-by: Alexandria C <alexandria.cortez@linuxandria.com>

--- descendant_guacamole.mk
+++ descendant_guacamole.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/guacamole/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/descendant/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_guacamole
+PRODUCT_NAME := descendant_guacamole
 PRODUCT_DEVICE := guacamole
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := OnePlus 7 Pro
@@ -37,3 +37,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="OnePlus7Pro-user 10 QKQ1.190716.003 1909110008 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+$(call inherit-product, device/oneplus/guacamole/GApps-flags.mk)
